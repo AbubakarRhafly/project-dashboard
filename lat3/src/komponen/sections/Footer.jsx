70e6b18b6280f521cde5805ec9d38ef747e66036
AbubakarRhafly <abubakarr.r.e.p@gmail.com>
2025-10-04 00:14:36 +0700

    red line debugging

--- lat3/src/komponen/sections/Footer.jsx
+++ lat3/src/komponen/sections/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
     <footer className="py-6 text-center text-sm text-slate-500 dark:text-slate-400">
-        Built with ❤️ using Atomic Design
+        capee pakkk saya stresss 
     </footer>
     );
 }
