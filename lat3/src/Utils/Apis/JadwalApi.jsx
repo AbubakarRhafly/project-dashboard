4152085f620dcda76e5fd305de4b6d6209f9c5ef
AbubakarRhafly <abubakarr.r.e.p@gmail.com>
2025-12-05 10:57:18 +0700

    update

--- lat3/src/Utils/Apis/JadwalApi.jsx
+++ lat3/src/Utils/Apis/JadwalApi.jsx
@@ -1,7 +1,7 @@
 import api from "../../Utils/AxiosInstance";
 
-export const getAllJadwal = () => api.get("/Jadwal");
-export const getJadwal    = (id) => api.get(`/Jadwal/${id}`);
-export const storeJadwal  = (data) => api.post("/Jadwal", data);
-export const updateJadwal = (id, data) => api.put(`/Jadwal/${id}`, data);
-export const deleteJadwal = (id) => api.delete(`/Jadwal/${id}`);
+export const getAllJadwal = () => api.get("/jadwal");
+export const getJadwal    = (id) => api.get(`jadwal/${id}`);
+export const storeJadwal  = (data) => api.post("/jadwal", data);
+export const updateJadwal = (id, data) => api.put(`/jadwal/${id}`, data);
+export const deleteJadwal = (id) => api.delete(`/jadwal/${id}`);
--- lat3/src/Utils/AxiosInstance.jsx
+++ lat3/src/Utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "http://localhost:8001",
     headers: { "Content-Type": "application/json" },
 });
 
