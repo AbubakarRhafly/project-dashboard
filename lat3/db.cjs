be5ab04ba6e275312981991e57a9f8ffac860752
AbubakarRhafly <abubakarr.r.e.p@gmail.com>
2025-12-05 10:02:50 +0700

    copy

---
+++ lat3/db.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    users: require("./db/users.json"),
+    mahasiswa: require("./db/mahasiswa.json"),
+    dosen: require("./db/dosen.json"),
+    matakuliah: require("./db/matakuliah.json"),
+    jadwal: require("./db/jadwal.json"),
+};
